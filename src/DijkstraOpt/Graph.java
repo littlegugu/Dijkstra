125ddc894f9473acaf4bf61c470ac855ed4e7f5c
Rain_ye3939 <305171397@qq.com>
2018-09-19 20:44:30 +0800

    相同一起拿完成。

--- src/DijkstraOpt/Graph.java
+++ src/DijkstraOpt/Graph.java
@@ -1,12 +1,13 @@
-package Dijkstra;
+package DijkstraOpt;
 
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
 
 public class Graph {
-    private int point;
-    public int[][] matrix;
+    private static int point;
+    public  static int[][] matrix;
     private static Random ran = new Random();
 
 
@@ -15,12 +16,12 @@ public class Graph {
         this.point = point;
     }
 
-    public int[][] randomGraph(){
+    public static int[][] randomGraph(){
         int row,col;
-        matrix=new int[this.point][this.point];
+        matrix=new int[point][point];
         int num;
-        for (row = 0; row < this.point; row++) {
-            for (col = row; col < this.point; col++) {
+        for (row = 0; row < point; row++) {
+            for (col = row; col < point; col++) {
                 if(col==row) {
                     matrix[col][row] = -1;
                 }else{
@@ -44,16 +45,15 @@ public class Graph {
         }
     }
 
-    public int[] dijkstra(int index){
+    public static int[] dijkstra(int index){
         ArrayList<Integer> t = new ArrayList<Integer>();//在此Arraylist里面不再查找
         t.add(index);
         int arr[]  = matrix[index].clone();//用于选当前指针的数组
-//        int[] dist={matrix[index].length};//最短路径数组
         int[] dist = new int[point];
         dist[index] = -1;
         int pointer;
         while (t.size()!=point) {
-            pointer       = minIndex(arr);
+            pointer       = minIndexf(arr).get(0);
             dist[pointer] = arr[pointer];//获得最短路径
             arr[pointer]  = -1;//改值，以防查找
             arr = update(arr,pointer,dist[pointer]);
@@ -61,7 +61,7 @@ public class Graph {
         }
         return dist;
     }
-    private int[] update(int[] arr,int pointer,int dist){
+    private static int[] update(int[] arr,int pointer,int dist){
         for (int col = 0; col < point; col++) {
             if ((matrix[pointer][col] != 0 && arr[col]>=0) &&((dist+matrix[pointer][col])<arr[col]) )
                 arr[col] = dist+matrix[pointer][col];
@@ -69,18 +69,52 @@ public class Graph {
         return  arr;
     }
 
-    private int minIndex(int[] arr){
+    private static ArrayList<Integer> minIndexf(int[] arr){
         int index;
         int minNum   = arr[0];
         int minIndex = 0;
+        ArrayList<Integer> minList = new  ArrayList<Integer>();
+        minList.add(minIndex);
         for (index = 1; index < arr.length; index++) {
             if (arr[index] > 0 && (minNum < 0 ||minNum>arr[index])) {
                 minNum   = arr[index];
                 minIndex = index;
+                minList.clear();
+                minList.add(minIndex);
+            }else if(arr[index] > 0 && arr[index]==minNum){
+                minList.add(index);
             }
         }
-        return minIndex;
+        return minList;
     }
+
+    public static int[] dijkstraOpt(int index){
+        ArrayList<Integer> t = new ArrayList<Integer>();//在此Arraylist里面不再查找
+        t.add(index);
+        int arr[]  = matrix[index].clone();//用于选当前指针的数组
+        int[] dist = new int[point];
+        dist[index] = -1;
+        int pointer;
+        int count = 0;
+        ArrayList<Integer> pArr;
+        while (t.size()!=point) {
+            pArr = minIndexf(arr);
+//            optupdate(arr,pArr,dist);
+            Iterator<Integer> pArrIt = pArr.iterator();
+            count++;
+            while (pArrIt.hasNext()){
+                pointer       = pArrIt.next();
+                dist[pointer] = arr[pointer];//获得最短路径
+                arr[pointer]  = -1;//改值，以防查找
+                t.add(pointer);
+                update(arr,pointer,dist[pointer]);
+            }
+        }
+//        System.out.println(count);
+        return dist;
+    }
+
+
 
 
 
